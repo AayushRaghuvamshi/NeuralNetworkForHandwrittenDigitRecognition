b34f1ea19cc6a8311f83742a7f48bd56e72188da
Aayush Raghuvamshi <aayush.raghuvamshi@gmail.com>
2024-01-04 17:40:35 -0800

    finished script for training a neural network and accurately predicting on test data from MNIST dataset

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0 # Normalize the images to have pixel values between 0 and 1
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test, y_test, verbose=2)
+
+predictions = model.predict(x_test)
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_test[i], cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions[i])
+    true_label = y_test[i]
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+    plt.xlabel(f"Predicted: {predicted_label}, True: {true_label}", color=color)
+plt.show()
+
+
